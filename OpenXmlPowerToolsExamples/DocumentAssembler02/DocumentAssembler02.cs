76e7cb14541f57eecc01edbeff575fe2c8961b34
Eric White <eric@ericwhite.com>
2015-08-13 14:12:20 +0530

    Enhance DocumentAssembler.cs so that it will alternatively process metadata stored in content controls

---
+++ OpenXmlPowerToolsExamples/DocumentAssembler02/DocumentAssembler02.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using OpenXmlPowerTools;
+
+namespace OpenXmlPowerTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo templateDoc = new FileInfo("../../TemplateDocument.docx");
+            FileInfo dataFile = new FileInfo("../../Data.xml");
+
+            WmlDocument wmlDoc = new WmlDocument(templateDoc.FullName);
+            XElement data = XElement.Load(dataFile.FullName);
+            bool templateError;
+            WmlDocument wmlAssembledDoc = DocumentAssembler.AssembleDocument(wmlDoc, data, out templateError);
+            if (templateError)
+            {
+                Console.WriteLine("Errors in template.");
+                Console.WriteLine("See AssembledDoc.docx to determine the errors in the template.");
+            }
+
+            FileInfo assembledDoc = new FileInfo("../../AssembledDoc.docx");
+            wmlAssembledDoc.SaveAs(assembledDoc.FullName);
+        }
+    }
+}
