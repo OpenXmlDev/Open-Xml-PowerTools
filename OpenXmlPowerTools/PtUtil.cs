2c22e97ed70b390fb7330f020b228345d09a7b0d
EricWhiteDev <eric@ericwhite.com>
2017-03-21 14:31:20 -0700

    Add Normalizer class

--- OpenXmlPowerTools/PtUtil.cs
+++ OpenXmlPowerTools/PtUtil.cs
@@ -27,6 +27,7 @@ using System.Linq;
 using System.Text;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.Schema;
 
 namespace OpenXmlPowerTools
 {
@@ -63,6 +64,142 @@ namespace OpenXmlPowerTools
             XElement newXElement = XElement.Parse(newElement);
             newXElement.Attributes().Where(a => a.IsNamespaceDeclaration).Remove();
             partXDoc.Root.Add(newXElement);
+        }
+    }
+
+    public class Normalizer
+    {
+        public static XDocument Normalize(XDocument source, XmlSchemaSet schema)
+        {
+            bool havePSVI = false;
+            // validate, throw errors, add PSVI information
+            if (schema != null)
+            {
+                source.Validate(schema, null, true);
+                havePSVI = true;
+            }
+            return new XDocument(
+                source.Declaration,
+                source.Nodes().Select(n =>
+                {
+                    // Remove comments, processing instructions, and text nodes that are
+                    // children of XDocument.  Only white space text nodes are allowed as
+                    // children of a document, so we can remove all text nodes.
+                    if (n is XComment || n is XProcessingInstruction || n is XText)
+                        return null;
+                    XElement e = n as XElement;
+                    if (e != null)
+                        return NormalizeElement(e, havePSVI);
+                    return n;
+                }
+                )
+            );
+        }
+
+        public static bool DeepEqualsWithNormalization(XDocument doc1, XDocument doc2,
+            XmlSchemaSet schemaSet)
+        {
+            XDocument d1 = Normalize(doc1, schemaSet);
+            XDocument d2 = Normalize(doc2, schemaSet);
+            return XNode.DeepEquals(d1, d2);
+        }
+
+        private static IEnumerable<XAttribute> NormalizeAttributes(XElement element,
+            bool havePSVI)
+        {
+            return element.Attributes()
+                    .Where(a => !a.IsNamespaceDeclaration &&
+                        a.Name != Xsi.schemaLocation &&
+                        a.Name != Xsi.noNamespaceSchemaLocation)
+                    .OrderBy(a => a.Name.NamespaceName)
+                    .ThenBy(a => a.Name.LocalName)
+                    .Select(
+                        a =>
+                        {
+                            if (havePSVI)
+                            {
+                                var dt = a.GetSchemaInfo().SchemaType.TypeCode;
+                                switch (dt)
+                                {
+                                    case XmlTypeCode.Boolean:
+                                        return new XAttribute(a.Name, (bool)a);
+                                    case XmlTypeCode.DateTime:
+                                        return new XAttribute(a.Name, (DateTime)a);
+                                    case XmlTypeCode.Decimal:
+                                        return new XAttribute(a.Name, (decimal)a);
+                                    case XmlTypeCode.Double:
+                                        return new XAttribute(a.Name, (double)a);
+                                    case XmlTypeCode.Float:
+                                        return new XAttribute(a.Name, (float)a);
+                                    case XmlTypeCode.HexBinary:
+                                    case XmlTypeCode.Language:
+                                        return new XAttribute(a.Name,
+                                            ((string)a).ToLower());
+                                }
+                            }
+                            return a;
+                        }
+                    );
+        }
+
+        private static XNode NormalizeNode(XNode node, bool havePSVI)
+        {
+            // trim comments and processing instructions from normalized tree
+            if (node is XComment || node is XProcessingInstruction)
+                return null;
+            XElement e = node as XElement;
+            if (e != null)
+                return NormalizeElement(e, havePSVI);
+            // Only thing left is XCData and XText, so clone them
+            return node;
+        }
+
+        private static XElement NormalizeElement(XElement element, bool havePSVI)
+        {
+            if (havePSVI)
+            {
+                var dt = element.GetSchemaInfo();
+                switch (dt.SchemaType.TypeCode)
+                {
+                    case XmlTypeCode.Boolean:
+                        return new XElement(element.Name,
+                            NormalizeAttributes(element, havePSVI),
+                            (bool)element);
+                    case XmlTypeCode.DateTime:
+                        return new XElement(element.Name,
+                            NormalizeAttributes(element, havePSVI),
+                            (DateTime)element);
+                    case XmlTypeCode.Decimal:
+                        return new XElement(element.Name,
+                            NormalizeAttributes(element, havePSVI),
+                            (decimal)element);
+                    case XmlTypeCode.Double:
+                        return new XElement(element.Name,
+                            NormalizeAttributes(element, havePSVI),
+                            (double)element);
+                    case XmlTypeCode.Float:
+                        return new XElement(element.Name,
+                            NormalizeAttributes(element, havePSVI),
+                            (float)element);
+                    case XmlTypeCode.HexBinary:
+                    case XmlTypeCode.Language:
+                        return new XElement(element.Name,
+                            NormalizeAttributes(element, havePSVI),
+                            ((string)element).ToLower());
+                    default:
+                        return new XElement(element.Name,
+                            NormalizeAttributes(element, havePSVI),
+                            element.Nodes().Select(n => NormalizeNode(n, havePSVI))
+                        );
+                }
+            }
+            else
+            {
+                return new XElement(element.Name,
+                    NormalizeAttributes(element, havePSVI),
+                    element.Nodes().Select(n => NormalizeNode(n, havePSVI))
+                );
+            }
         }
     }
 
@@ -880,7 +1017,7 @@ namespace OpenXmlPowerTools
             LastTime = now;
         }
 
-        public static string DumpBuckets()
+        public static string DumpBucketsByKey()
         {
             StringBuilder sb = new StringBuilder();
             foreach (var bucket in Buckets.OrderBy(b => b.Key))
@@ -888,7 +1025,25 @@ namespace OpenXmlPowerTools
                 string ts = bucket.Value.Time.ToString();
                 if (ts.Contains('.'))
                     ts = ts.Substring(0, ts.Length - 5);
-                string s = bucket.Key.PadRight(60,'-') + "  " + string.Format("{0:00000000}", bucket.Value.Count) + "  " + ts;
+                string s = bucket.Key.PadRight(60, '-') + "  " + string.Format("{0:00000000}", bucket.Value.Count) + "  " + ts;
+                sb.Append(s + Environment.NewLine);
+            }
+            TimeSpan total = Buckets
+                .Aggregate(TimeSpan.Zero, (t, b) => t + b.Value.Time);
+            var tz = total.ToString();
+            sb.Append(string.Format("Total: {0}", tz.Substring(0, tz.Length - 5)));
+            return sb.ToString();
+        }
+
+        public static string DumpBucketsByTime()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var bucket in Buckets.OrderBy(b => b.Value.Time))
+            {
+                string ts = bucket.Value.Time.ToString();
+                if (ts.Contains('.'))
+                    ts = ts.Substring(0, ts.Length - 5);
+                string s = bucket.Key.PadRight(60, '-') + "  " + string.Format("{0:00000000}", bucket.Value.Count) + "  " + ts;
                 sb.Append(s + Environment.NewLine);
             }
             TimeSpan total = Buckets
@@ -917,5 +1072,13 @@ namespace OpenXmlPowerTools
                 writer.WriteEntityRef(this.Value);
         }
         public XEntity(string value) : base(value) { }
+    }
+
+    public static class Xsi
+    {
+        public static XNamespace xsi = "http://www.w3.org/2001/XMLSchema-instance";
+
+        public static XName schemaLocation = xsi + "schemaLocation";
+        public static XName noNamespaceSchemaLocation = xsi + "noNamespaceSchemaLocation";
     }
 }
