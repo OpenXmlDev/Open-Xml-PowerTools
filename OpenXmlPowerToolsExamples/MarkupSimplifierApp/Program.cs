cd55eb609a36358ef96ec9bbbf618ede5698d7c0
Stefan Seeland <168659+stesee@users.noreply.github.com>
2020-03-29 17:33:14 +0200

    Migrated example app to something that is compatible with no win os

--- OpenXmlPowerToolsExamples/MarkupSimplifierApp/Program.cs
+++ OpenXmlPowerToolsExamples/MarkupSimplifierApp/Program.cs
@@ -1,24 +1,46 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+﻿using DocumentFormat.OpenXml.Packaging;
+using MarkupSimplifierApp.Properties;
+using OpenXmlPowerTools;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
 
 namespace MarkupSimplifierApp
 {
-    static class Program
+    internal class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        private static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Example output files are in a DateTime stamped directory in ./bin/debug.  The directory name is ExampleOutput-yy-mm-dd-hhmmss.");
+                Console.WriteLine("If you are building in release mode, they will, of course, be in ./bin/release.");
+                Console.WriteLine("MarkupSimplifierApp.exe 1.docx 2.docx");
+            }
+
+            foreach (var item in args)
+            {
+                using (var doc =
+                    WordprocessingDocument.Open(item, true))
+                {
+                    var settings = new SimplifyMarkupSettings
+                    {
+                        RemoveContentControls = Settings.Default.RemoveContentControls,
+                        RemoveSmartTags = Settings.Default.RemoveSmartTags,
+                        RemoveRsidInfo = Settings.Default.RemoveRsidInfo,
+                        RemoveComments = Settings.Default.RemoveComments,
+                        RemoveEndAndFootNotes = Settings.Default.RemoveEndAndFootNotes,
+                        ReplaceTabsWithSpaces = Settings.Default.ReplaceTabsWithSpaces,
+                        RemoveFieldCodes = Settings.Default.RemoveFieldCodes,
+                        RemovePermissions = Settings.Default.RemovePermissions,
+                        RemoveProof = Settings.Default.RemoveProof,
+                        RemoveSoftHyphens = Settings.Default.RemoveSoftHyphens,
+                        RemoveLastRenderedPageBreak = Settings.Default.RemoveLastRenderedPageBreak,
+                        RemoveBookmarks = Settings.Default.RemoveBookmarks,
+                        RemoveWebHidden = Settings.Default.RemoveWebHidden,
+                        NormalizeXml = Settings.Default.NormalizeXml,
+                    };
+                    MarkupSimplifier.SimplifyMarkup(doc, settings);
+                }
+            }
         }
     }
 }
