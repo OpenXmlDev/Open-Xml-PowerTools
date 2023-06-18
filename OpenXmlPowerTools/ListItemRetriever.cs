0ccae9a4bc0b742912172526bf645074ef9d94b5
alexeysp11 <alexeyspiridon1111@gmail.com>
2023-06-18 13:47:43 +0500

    added ListItemTextGetter_es_ES

--- OpenXmlPowerTools/ListItemRetriever.cs
+++ OpenXmlPowerTools/ListItemRetriever.cs
@@ -19,6 +19,7 @@ namespace OpenXmlPowerTools
                 {"tr-TR", ListItemTextGetter_tr_TR.GetListItemText},
                 {"ru-RU", ListItemTextGetter_ru_RU.GetListItemText}, 
                 {"de-DE", ListItemTextGetter_de_DE.GetListItemText}, 
+                {"es-ES", ListItemTextGetter_es_ES.GetListItemText}, 
                 {"sv-SE", ListItemTextGetter_sv_SE.GetListItemText},
                 {"zh-CN", ListItemTextGetter_zh_CN.GetListItemText},
             };
